b91f17e094073d13aad12c357c8c216119bc4eb2
Mike <mixa_and_java@mail.ru>
2023-07-25 01:38:28 +0300

    ошибок нет.почему просто не использовать встроенное форматирование "ctrl+alt+l" ?

--- src/main/java/ru/job4j/Info.java
+++ src/main/java/ru/job4j/Info.java
@@ -3,7 +3,7 @@ package ru.job4j;
 public class Info {
     public static void main(String[] args) {
 
-        System.out.println("25.07.2023 ");
+        System.out.println("25.07.2023");
 
     }
 }
